5203cd3075cc9e8033931bb0085b76fc43e548af
kirster <kir.sazonov@gmail.com>
2018-01-31 14:07:33 +0300

    fix AnswersController specs

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 describe AnswersController do
+  let(:question) { create(:question) }
   let(:answer) { create(:answer) }
 
   describe 'GET #new' do
-    before { get :new, params: { question_id: answer.question_id }}
+    before { get :new, params: { question_id: question }}
 
     it 'assigns a new Answer to @answer' do
       expect(assigns(:answer)).to be_a_new(Answer)
@@ -28,16 +29,14 @@ describe AnswersController do
   end
 
   describe 'POST #create' do
-    before { answer }
-
     context 'with valid attributes' do
       it 'saves new answer to database' do
         expect { post :create, params: { answer: attributes_for(:answer), 
-                                      question_id: answer.question_id } }.to change(Answer, :count).by(1)
+                                      question_id: question } }.to change(question.answers, :count).by(1)
       end
 
       it 'redirects to show view' do
-        post :create, params: { answer: attributes_for(:answer), question_id: answer.question_id }
+        post :create, params: { answer: attributes_for(:answer), question_id: question }
         expect(response).to redirect_to answer_path(assigns(:answer))
       end  
     end
@@ -45,11 +44,11 @@ describe AnswersController do
    context 'with invalid attributes' do
     it 'doesn`t save answer to database' do
       expect { post :create, params: { answer: attributes_for(:invalid_answer), 
-                                      question_id: answer.question_id } }.to_not change(Answer, :count)
+                                      question_id: question } }.to_not change(question.answers, :count)
     end
 
     it 'again renders new view' do
-      post :create, params: { answer: attributes_for(:invalid_answer), question_id: answer.question_id }
+      post :create, params: { answer: attributes_for(:invalid_answer), question_id: question }
       expect(response).to render_template :new
     end
    end 
