ea17ae1b3588d2bc32f8b7a31a6ed6f9e150f7f4
kirster <kir.sazonov@gmail.com>
2018-02-01 19:01:14 +0300

    add questions index acceptance test

---
+++ spec/acceptance/questions/index_question_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature 'User can view all questions', %q{
+  In order to know actual problems of other users
+  As a User
+  I want to be able to read questions 
+} do
+
+  given(:user) { create(:user) }
+  let!(:questions) { create_list(:question, 3) }
+
+  scenario 'Logged user can view questions' do
+    questions = create_list(:question, 3)
+    sign_in(user)
+
+    visit questions_path
+    questions.each { |question| expect(page).to have_content question.title }   
+  end
+
+  scenario 'Guest can view questions' do
+    visit questions_path
+    questions.each { |question| expect(page).to have_content question.title }    
+  end
+end 
