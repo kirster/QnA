63ea823c6642acffd7667bf1cd9e458e76eeab10
kirster <kir.sazonov@gmail.com>
2018-02-01 19:43:24 +0300

    add questions show acceptance test

---
+++ spec/acceptance/questions/show_question_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'User can view particular question', %q{
+  In order to read particular question
+  As a User
+  I want to be able to read question and it`s answers 
+} do
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given(:answers) { create_list(:answer, 2, question: question)}
+
+  scenario 'Logged user can view question' do
+    sign_in(user)
+
+    visit question_path(question)
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+    answers.each { |answer| expect(page).to have_content answer.body }
+  end
+
+  scenario 'Guest can view question' do
+    visit question_path(question)
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+    answers.each { |answer| expect(page).to have_content answer.body } 
+  end
+
+end
